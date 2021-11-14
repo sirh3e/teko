c6ff4bea4ab68eab1d595a2282ea4506ec8d3a4a
sirh3e <sirh3e@protonmail.com>
2021-11-14 20:17:15 +0100

    added commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,40 @@
 import core.Exercise;
+import core.Result;
 import exercises.Exercise01;
 
 public class Main {
     public static void main(String[] args) {
         Exercise exercise = new Exercise01();
         Exercise.Executor.execute(exercise);
+
+        var heise = Result.Factory.fromOk("");
+
+        var result = parseToInt("");
+/*
+        var text = "42";
+        var result = parseToInt(text);
+        var r1 = result
+                .map(Main::add5)
+                .map(Main::add5)
+                .map(Main::add5)
+                .map(Main::add5)
+                ;
+        var number= r1.unwrap();
+
+        System.out.println(number);
+ */
+    }
+
+    public static Result<Integer, String> parseToInt(String text){
+        try{
+            var number =Integer.parseInt(text);
+            return Result.Factory.fromOk(number);
+        }catch (NumberFormatException exception){
+            return Result.Factory.fromErr(text);
+        }
+    }
+
+    public static Integer add5(Integer number) {
+        return number + 5;
     }
 }
---
+++ src/core/Result.java
@@ -0,0 +1,57 @@
+package core;
+
+import java.util.function.Function;
+
+public class Result<TOk, TErr> {
+
+    private final TOk _ok;
+    private final boolean _isOk;
+    private final TErr _err;
+
+    private Result(TOk ok, TErr err) {
+        if(ok != null){
+            this._ok = ok;
+            this._isOk = true;
+            this._err = null;
+        }else{
+            this._ok = null;
+            this._isOk = false;
+            this._err = err;
+        }
+    }
+
+    public <T> Result<T, TErr> map(Function<TOk, T> map){
+        return this._isOk
+                ? Factory.fromOk(map.apply(this._ok))
+                : Factory.fromErr(this._err);
+    }
+
+    public boolean isOk() {
+        return this._isOk;
+    }
+
+    public boolean isErr(){
+        return !isOk();
+    }
+
+    public TOk unwrap(){
+        if(this._isOk){
+            return this._ok;
+        }
+        throw new RuntimeException();
+    }
+
+    public TOk unwrapOr(TOk ok){
+        return isOk() ? this._ok : ok;
+    }
+
+    public final static class Factory{
+        public static <TOk, TErr> Result<TOk, TErr> fromOk(TOk ok){
+            return new Result<TOk, TErr>(ok, null);
+        }
+
+        public static <TOk, TErr> Result<TOk, TErr> fromErr(TErr err){
+            return new Result<TOk, TErr>(null, err);
+        }
+    }
+}
